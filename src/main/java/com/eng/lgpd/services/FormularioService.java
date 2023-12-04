27f14ac9048f003a965d7e893bb36f99971e2c9a
Leonardo Naime <leonardonaime2@gmail.com>
2023-12-04 18:59:09 -0300

    adição getbyid e getall formulários

--- src/main/java/com/eng/lgpd/services/FormularioService.java
+++ src/main/java/com/eng/lgpd/services/FormularioService.java
@@ -1,5 +1,10 @@
 package com.eng.lgpd.services;
 
+import java.util.List;
+import java.util.Optional;
+
+
+import org.hibernate.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,4 +21,14 @@ public class FormularioService {
     public Formulario criarFormulario(Formulario formulario) {
         return formularioRepository.save(formulario);
     }
+
+    public List<Formulario> findAll() {
+        return formularioRepository.findAll();
+    }
+
+    public Formulario findById(Long id) {
+        Optional<Formulario> obj = formularioRepository.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado!", null));
+    }
+
 }
